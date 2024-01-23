1c12ca56e6164a755fc1b8f86af88d84037528fd
codestudent24 <belgorod2407@yandex.ru>
2024-01-23 22:46:42 +1000

    feat: add base url

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'https://codestudent24.github.io/portfolio/',
   plugins: [react()],
 })
